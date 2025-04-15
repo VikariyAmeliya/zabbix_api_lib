f8b87470038efea94473271aa4f5cddd84afcad5
VikariyAmeliya <166492712+VikariyAmeliya@users.noreply.github.com>
2025-04-15 16:11:10 +0300

    Update make_cache.py

--- make_cache.py
+++ make_cache.py
@@ -5,7 +5,7 @@ import json
 import time
 
 
-class ZabbixCacheBuilder:
+class ZabbixCache:
     def __init__(self, api, cache_dir="/Users/whoami?/Documents/_zabbix/cache_structure", 
                  pid_dir="/Users/whoami?/Documents/_zabbix/pid"):
 
@@ -19,7 +19,7 @@ class ZabbixCacheBuilder:
         self.pid_dir.mkdir(parents=True, exist_ok=True)
         
     def _check_proc(self):
-        if self.pid_file.exists:
+        if self.pid_file.exists():
             print(f"Процесс уже запущен, файл {self.pid_file} уже существует")
             return True
         return False
@@ -35,11 +35,45 @@ class ZabbixCacheBuilder:
     def make_cache(self):
         if self._check_proc():
             return False
+        
         try:
             self._create_pid()
             start_time = time.time()
-            users = self.api.get_usergroup()
-            hostrgroups = self.api.get_hostgroup_list_v64()
+            
+            work = {
+                'users': {},
+                'hg_available': {},
+                'host': {}
+            }
+            
+            try:
+                work['users'] = self.api.get_usergroup()
+                work['hg_available'] = self.api.get_hostgroup_list_v64()
+                work['host'] = self.api.get_hostgroup_hosts_v64(groups=work['hg_available'])
+                
+                if 'array' in work['host']:
+                    for hostid in work['host']['all'].keys():
+                        tags = self.api.get_host_tags([hostid])
+                        if tags and 'host' in tags and hostid in tags['host']:
+                            work['host']['all'][hostid]['tags'] = tags['host'][hostid]('tags', {})
+                             
+                with open(self.cache_file, 'w') as f:
+                    json.dump(work, f, indent=2)
+                    
+                exec_time = time.time() - start_time
+                print(f" {exec_time} seconds")
+                return True
+            
+            except Exception as e:
+                print(f"Ошибка при создании кэша: {e}")
+                return False
+            
+        finally:
+            self._remove_pid()
+            print("PID-файл удален")
+
+           
+ 
             
         
         
